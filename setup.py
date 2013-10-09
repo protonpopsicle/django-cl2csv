a1edd2dd393a13002de47c363720d2ced6c976de
Rhizome.org <smeisburger@gmail.com>
2013-10-09 14:35:02 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-cl2csv',
+      version='1.0',
+      description='CL2CSV (Change List to CSV) is a Django app (pluggable) that makes change lists (in the admin) exportable.',
+      author='Scott Meisburger',
+      author_email='smeisburger@gmail.com',
+      url='https://github.com/protonpopsicle/django-cl2csv',
+      packages=['2csv],
+     )
